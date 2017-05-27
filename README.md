f2aedab21da421a7f3a8bd6dc158eb77c325b3b3
mattjmcnaughton <mattjmcnaughton@gmail.com>
2017-05-27 23:50:34 +0000

    Initial commit
    The initial commit of `btstrp` - a project
    which makes it easy to provision a computer
    to my specifications.

---
+++ README.md
@@ -0,0 +1,11 @@
+# btstrp
+
+`btstrp` works to easily configure a new unix computer
+to my specifications.
+
+## Usage
+
+- `chmod +x install.sh && ./install.sh`
+- `ansible-playbook playbooks/main.yml -K -e "hostname=$HOSTNAME github_access_token=$GITHUB_ACCESS_TOKEN"`
+
+
---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Install the dependencies necessary for running
+# the main `playbook.yml` which configures the
+# machine.
+# Currently this only supports Linux systems with
+# `apt` installed.
+
+set -e
+
+KERNEL=`uname -a | cut -d " " -f1`
+
+if [ "$KERNEL" = "Linux" ]
+then
+    if which apt
+    then
+        sudo apt-get install software-properties-common
+        sudo apt-add-repository ppa:ansible/ansible
+        sudo apt-get update
+        sudo apt-get install ansible
+    else
+        exit 1
+    fi
+elif [ "$KENEL" = "Darwin" ]
+then
+    exit 1
+else
+    exit 1
+fi
+
