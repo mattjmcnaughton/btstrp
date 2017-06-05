1e1d5c33dc5723e93403d58fd808ebc9da4e61b4
mattjmcnaughton <mattjmcnaughton@gmail.com>
2017-06-05 09:17:56 -0400

    Configure xinput mouse on i3 start
    Add a utility script for configuring my mouse to
    use reverse scrolling and be less sensitive. Set up
    i3 to run this script on boot.
    
    Using i3 appears to be the best method for running this script.
    I also tried `~/.profile`, a `reboot` cron, and `~/.xinitrc`,
    and none of them appeared to actually call the script when
    I wanted.
    
    I left the option of specifying a `~/.profile.local` in case
    I decide that could be useful in the future.

---
+++ files/bash/utility_script/xinput_configuration.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+# Configure any connected mouse devices with decreased acceleration
+# and natural scrolling.
+#
+# Most effectively run on system login (i.e. called from `~/.profile`.
+#
+# Usage:
+#     xinput_configuration.sh
+
+# Currently this only works with my `Anker` mouse. Update to use a more
+# generic mapping between settings amd values for different types of mouses.
+configure_input() {
+    while [ $# -gt 0 ]
+    do
+        accel_setting="Device Accel Constant Deceleration"
+        accel_value="2.0"
+        if xinput --list-props "$1" | grep -q "$accel_setting"
+        then
+            xinput --set-prop "$1" "$accel_setting" $accel_value
+        fi
+
+        scrolling_setting="Evdev Scrolling Distance"
+        scrolling_value="-1, -1, -1"
+        if xinput --list-props "$1" | grep -q "$scrolling_setting"
+        then
+            xinput --set-prop "$1" "$scrolling_setting" $scrolling_value
+        fi
+
+        shift
+    done
+}
+
+export -f configure_input
+
+xinput --list |
+    grep -E ".*slave.*pointer.*" |
+        tr -cd '[:alnum:][:punct:][:space:]' |
+            sed -e 's/\s\+/ /g' -e 's/^\s//g' |
+                grep -Eo ".*id=" |
+                    sed -e 's/\s\+id=//g' |
+                        xargs -d '\n' bash -c 'configure_input "$@"' "$0"
