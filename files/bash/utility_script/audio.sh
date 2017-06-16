d9160beffc5494c6d5165a8d6802d368d6756b31
mattjmcnaughton <mattjmcnaughton@gmail.com>
2017-06-16 16:44:38 -0400

    Add `audio.sh` script for controlling amixer
    Add a wrapper script around the `amixer` cli to make it easier
    to modify the system volume.

---
+++ files/bash/utility_script/audio.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+#
+# A wrapper around volume management. This script makes it simple to control the
+# sound from the command line.
+#   audio.sh [COMMAND] [ARGUMENTS]
+
+set -e
+
+DEFAULT_INCREMENT=5
+AMIXER_BASE="amixer -D pulse"
+AMIXER_SET="$AMIXER_BASE sset Master"
+
+audio::mute() {
+    bash -c "$AMIXER_SET 0\% mute"
+}
+
+audio::unmute() {
+    bash -c "$AMIXER_SET 0\% unmute"
+}
+
+audio::up() {
+    bash -c "$AMIXER_SET $1\%+"
+}
+
+audio::down() {
+    bash -c "$AMIXER_SET $1\%-"
+}
+
+audio::status() {
+    bash -c "$AMIXER_BASE get Master"
+}
+
+audio::usage() {
+    echo "audio.sh [COMMAND] [ARGUMENTS]"
+}
+
+case $1 in
+mute)
+    audio::mute
+    ;;
+unmute)
+    audio::unmute
+    ;;
+up)
+    if [ $# -eq 2 ]
+    then
+        audio::up "$2"
+    else
+        audio::up "$DEFAULT_INCREMENT"
+    fi
+    ;;
+down)
+    if [ $# -eq 2 ]
+    then
+        audio::down "$2"
+    else
+        audio::down "$DEFAULT_INCREMENT"
+    fi
+    ;;
+status)
+    audio::status
+    ;;
+help)
+    audio::usage
+    ;;
+*)
+    echo "Improper arguments passed to audio.sh"
+    audio::usage
+    exit 2
+    ;;
+esac
+
+exit 0
